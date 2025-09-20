cb72b39abfceb5f11eb92bb1a97247c4a03f2d58
Asherlar <daferloa@gmail.com>
2025-09-20 15:17:36 +0200

    Creada primera logica en backend con comunicacion con Frontend

--- BACKEND/FastAPI/main.py
+++ BACKEND/FastAPI/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "¡Hola FastAPI!"}
+# Permitir peticiones desde frontend local
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes restringir esto a tu frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/2")
-async def root2():
-    return {"message": "¡Cuánto tiempo!"}
+@app.get("/2/{kms}")
+def get_kms(kms: int):
+    color = "white"
+    msg = ""
+    if kms > 20000:
+        color = "red"
+        msg = "Cambio necesario"
+    else:
+        color = "green"
+        msg = "Cambio no necesario"
+    return {"color": color, "msg": msg}
